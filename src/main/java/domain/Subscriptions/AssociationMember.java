18f4c1d79edca70e9d635e1146b71c5d210c2769
Eitan-Chertok <eitan.chertok@ibm.com>
2022-05-13 17:10:35 +0300

    moved input and output from AssociationMember to Service Layer

--- src/main/java/domain/Subscriptions/AssociationMember.java
+++ src/main/java/domain/Subscriptions/AssociationMember.java
@@ -1,17 +1,4 @@
 package domain.Subscriptions;
-
-import dataBase.Loaders.LeagueLoader;
-import dataBase.Loaders.RefereeLoader;
-import dataBase.Loaders.SeasonLoader;
-import dataBase.Writers.RefereeWriter;
-import domain.LeagueComponents.League;
-import domain.LeagueComponents.Match;
-import domain.LeagueComponents.Season;
-import domain.SystemManagment;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class AssociationMember extends Subscription {
     public AssociationMember(String id, String name) {
         super(id, name);
